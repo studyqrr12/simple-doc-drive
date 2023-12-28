aebc5efe7ba6b34eff8bfc659a774719430c1b9d
studyqrr12 <studyqrr12@gmail.com>
2023-12-28 21:37:56 +0900

    Init SpringBoot 프로젝트 초기 파일

---
+++ src/main/java/com/studyqrr12/simpledocdrive/SimpleDocDriveApplication.java
@@ -0,0 +1,13 @@
+package com.studyqrr12.simpledocdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDocDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDocDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyqrr12/simpledocdrive/SimpleDocDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyqrr12.simpledocdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDocDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
